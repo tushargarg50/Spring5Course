c13ef14707edb6fd18eb12ab3553efd8135aceca
Tushar Garg <tushargarg50@gmail.com>
2020-09-02 09:21:06 +0530

    Initial Application Creation

---
+++ src/test/java/com/tg/spring5/Spring5CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tg.spring5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
